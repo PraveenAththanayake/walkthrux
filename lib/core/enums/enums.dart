9ba3b8ab327a4c3397453d6eba26b122b7aa749f
PraveenSilverline <aththanayakempa@gmail.com>
2025-06-03 22:32:19 +0530

    feat: create showcase_config model

---
+++ lib/core/enums/enums.dart
@@ -0,0 +1,3 @@
+enum ShowcaseShape { rectangle, circle, roundedRectangle, custom }
+
+enum TooltipPosition { top, bottom, left, right, auto }
---
+++ lib/core/models/showcase_config.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:walkthrux/core/enums/enums.dart';
+
+class ShowcaseConfig {
+  final String id;
+  final String title;
+  final String description;
+  final Widget? child;
+  final GlobalKey targetKey;
+  final TooltipPosition tooltipPosition;
+  final ShowcaseShape shape;
+  final Color overlayColor;
+  final double overlayOpacity;
+  final Duration animationDuration;
+  final Curve animationCurve;
+  final EdgeInsets padding;
+  final BorderRadius? borderRadius;
+  final bool enablePulseAnimation;
+  final Color pulseColor;
+  final Widget? customTooltip;
+  final String? audioPath;
+  final bool enableTTS;
+  final VoidCallback? onShow;
+  final VoidCallback? onDismiss;
+  final Map<String, dynamic>? analyticsData;
+
+  const ShowcaseConfig({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.targetKey,
+    this.child,
+    this.tooltipPosition = TooltipPosition.auto,
+    this.shape = ShowcaseShape.rectangle,
+    this.overlayColor = Colors.black,
+    this.overlayOpacity = 0.75,
+    this.animationDuration = const Duration(milliseconds: 300),
+    this.animationCurve = Curves.easeInOut,
+    this.padding = const EdgeInsets.all(8.0),
+    this.borderRadius,
+    this.enablePulseAnimation = true,
+    this.pulseColor = Colors.white,
+    this.customTooltip,
+    this.audioPath,
+    this.enableTTS = false,
+    this.onShow,
+    this.onDismiss,
+    this.analyticsData,
+  });
+}
